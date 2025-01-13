829c2d97129db4c97551016af1b85344c459bc31
Janari Stserbina <janari.stserbina@gmail.com>
2025-01-13 13:13:34 +0200

    Yl 3.

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,9 +1,21 @@
+import { useState, useEffect } from "react";
+
 const Meals = () => {
+    const [meals, setMeals] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3001/meals')
+        .then(res =>  res.json())
+        .then(data => setMeals(data))
+    }, [])
+
+    console.log(meals)
+
     return (
         <ul id="meals">
             { 
                 // list of meals
-                
+
             }
         </ul>
     )
